a22f0ca7b4088cb6bfe302cebb2bd1ed308759d1
Seokmin Hong <ghdtjrald240@gmail.com>
2025-04-29 16:43:13 +0900

    Migrate to a Rust project

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Emit the init script
+    Init,
+}
+
+fn main() {
+    let cli = Cli::parse();
+    match &cli.command {
+        Commands::Init => {
+            println!("{}", INIT_STR);
+        }
+    }
+}
+
+const INIT_STR: &str = include_str!("../init.fish");
